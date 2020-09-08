12543f97bc90841d73dd8f351451fb32490c12c0
abditimer <thatimer@gmail.com>
2020-09-08 20:54:56 +0100

    line change and new data downloaded

--- data_scripts/download_data.py
+++ data_scripts/download_data.py
@@ -21,7 +21,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-
 from selenium.webdriver.common.keys import Keys
 
 
