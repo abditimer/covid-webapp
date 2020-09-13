15cc00ac5dd502c73e29eb648c41a8c6e8b01a96
abditimer <thatimer@gmail.com>
2020-09-13 20:03:45 +0100

    added main check

--- covidwebapp/__init__.py
+++ covidwebapp/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 # __name__: (predefined variable) the name of the module in which it is used
 app = Flask(__name__)
-app.run()
+if __name__ == '__main__':
+    app.run()
 from covidwebapp import routes
